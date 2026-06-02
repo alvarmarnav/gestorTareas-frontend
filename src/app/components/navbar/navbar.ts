6f4c1fbb0755d101e8035de417eb5fe6c8d063dd
Alvaro M <alvarmarnav@gmail.com>
2026-06-02 16:56:40 +0200

    Añadir elementos adaptativos task-card. Mejorar Navbar. Quitar elementos del login

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,30 +1,37 @@
 import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { filter, map } from 'rxjs';
 import { AuthService } from '../../services/auth.service';
-import { filter, map, Observable } from 'rxjs';
-import {toSignal} from '@angular/core/rxjs-interop';
 @Component({
   selector: 'app-navbar',
-  standalone:true,
+  standalone: true,
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
   protected authService = inject(AuthService);
-private router = inject(Router);
+  private router = inject(Router);
 
-onLogout(): void {
-this.authService.logout();
-this.router.navigate(['/login']);
-}
-isLoginPage = toSignal(
+  onLogout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+  isLoginPage = toSignal(
     this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd),
-      map((event: any) => event.urlAfterRedirects === '/login')
+      filter((event) => event instanceof NavigationEnd),
+      map((event: any) => event.urlAfterRedirects === '/login'),
     ),
-    { initialValue: false }
+    { initialValue: false },
   );
+  menuOpen = false;
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu(): void {
+    this.menuOpen = false;
+  }
 }
-
-
